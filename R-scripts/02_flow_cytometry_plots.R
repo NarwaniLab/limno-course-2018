d59b2a3ec272872fc763bc7b499da5a570315754
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-10-24 12:45:56 +0200

    adding plate key and plots

---
+++ R-scripts/02_flow_cytometry_plots.R
@@ -0,0 +1,34 @@
+### Flow cytometry data visualization
+library(tidyverse)
+
+all_particles <- read_csv("data-processed/particles.csv")
+sample_key <- read_csv("data-raw/Limno-2018.csv")
+plate_key <- read_csv("data-raw/limno-96-well-plate-key.csv")
+
+plate_key2 <- plate_key %>% 
+	mutate(row = str_to_upper(row)) %>% 
+	mutate(column = formatC(column, width = 2, flag = 0)) %>% 
+	unite(col = well, row, column, sep = "")
+	
+
+
+all_fcs3_all <- all_particles %>% 
+	select(4:10) %>% 
+	dplyr::filter(fl1_a > 0, fl3_a > 0) 
+
+
+all_fcs3_all %>% 
+	dplyr::filter(well == "A04") %>% 
+	ggplot(aes(x = fl1_a, y = fl3_a)) + geom_point() + scale_y_log10() + scale_x_log10()
+
+ sorted_all <- all_fcs3_all %>% 
+	mutate(type = NA) %>% 
+	mutate(type = ifelse(fl3_a > 1250, "algae", type)) 
+
+all_sorted_all <- left_join(sorted_all, all_treatments, by = "well") %>% 
+	dplyr::filter(!grepl("A", well)) %>% 
+	mutate(plate = as.integer(plate))
+
+str(all_sorted_all)
+
+all_sorted2 <- left_join(all_sorted_all, plate_key, by = "plate")
